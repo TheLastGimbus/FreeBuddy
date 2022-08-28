5d1c175f66e537a7ea5e3b55dbc7d37820b75861
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-08-28 22:38:10 +0200

    Move everything to a function to enable smart-divide
    oh... and implement smart divide that finds multiple payloads in a single line and divides them

--- notes/live_print_data.py
+++ notes/live_print_data.py
@@ -2,8 +2,6 @@ import argparse
 import re
 from datetime import datetime
 
-import sys
-
 import print_hex_data
 
 p = argparse.ArgumentParser()
@@ -19,6 +17,8 @@ p.add_argument('--search-for-bytes', type=str, required=False,
 p.add_argument('--print-time', action='store_true')
 p.add_argument('--only-sent', action='store_true', help='Only print sent data')
 p.add_argument('--only-received', action='store_true', help='Only print received data')
+p.add_argument('--smart-divide', type=bool, default=True, help='Divide payloads into two if length byte says so.'
+                                                               'Also detects if they are duplicates and skips them.')
 args = p.parse_args()
 
 
@@ -29,51 +29,74 @@ def is_sublist(big_list, sublist):
     return False
 
 
+def handle_payload(raw_line: str, payload: str, is_send: bool, is_receive: bool, smart_divided: bool):
+    decimals = print_hex_data.string_to_decimals(payload)
+    chars = print_hex_data.decimals_to_chars(decimals)
+
+    length = decimals[2]
+    service_id = decimals[4]
+    command_id = decimals[5]
+    magic_bytes = decimals[0], decimals[1], decimals[3]
+    if magic_bytes != (90, 0, 0):
+        raise Exception(f'MAGIC BYTES NOT MAGIC!!!\nBytes:{magic_bytes}\n{payload}')
+
+    if (args.filter_service is not None and service_id != args.filter_service) \
+            or (args.filter_command is not None and command_id != args.filter_command) \
+            or (args.search_for_bytes is not None and not is_sublist(decimals, eval(args.search_for_bytes))) \
+            or (args.only_sent and not is_send) \
+            or (args.only_received and not is_receive) \
+            or (args.filter_length_max is not None and length > args.filter_length_max):
+        return
+
+    # print unix epoch
+    if args.print_time:
+        print(datetime.now().timestamp(), end=' ')
+    print("---Sent---:" if is_send else ("-Received-:" if is_receive else "UNKNOWN SOURCE: "))
+    if args.very_verbose:
+        print(raw_line)
+        if smart_divided:
+            print('(Psst: Actually parsed bytes were smartly divided/ignored thanks to --smart-divide option)')
+    if args.verbose:
+        print(payload)
+        print(decimals)
+
+    if args.only_print:
+        print(chars)
+        return
+
+    print('{ ServiceID:', service_id, 'CommandID:', command_id, '}')
+    print('Data:', decimals[6:-2])
+
+    if args.print:
+        print('=== Printable ===')
+        print(chars)
+        print('=================')
+    print()
+
+
 while True:
     line = sys.stdin.readline()
     # print if line matches regex r' data: '
     if re.search(r'(received length: \d+ data: )|(Send \[Len]: \d+ \[Data]: )', line):
-        is_send = bool(re.search(r'Send ', line))
-        is_receive = bool(re.search(r'received length: ', line))
-        res = re.search(r'5a.+', line)
+        _is_send = bool(re.search(r'Send ', line))
+        _is_receive = bool(re.search(r'received length: ', line))
+        res = re.search(r'5a\w+', line)
         data = res.group(0)
-        decimals = print_hex_data.string_to_decimals(data)
-        chars = print_hex_data.decimals_to_chars(decimals)
-
-        length = decimals[2]
-        service_id = decimals[4]
-        command_id = decimals[5]
-        magic_bytes = decimals[0], decimals[1], decimals[3]
-        if magic_bytes != (90, 0, 0):
-            raise Exception(f'MAGIC BYTES NOT MAGIC!!!\nBytes:{magic_bytes}\n{data}')
-
-        if (args.filter_service is not None and service_id != args.filter_service) \
-                or (args.filter_command is not None and command_id != args.filter_command) \
-                or (args.search_for_bytes is not None and not is_sublist(decimals, eval(args.search_for_bytes))) \
-                or (args.only_sent and not is_send) \
-                or (args.only_received and not is_receive) \
-                or (args.filter_length_max is not None and length > args.filter_length_max):
+        if not args.smart_divide:
+            handle_payload(data, _is_send, _is_receive)
             continue
 
-        # print unix epoch
-        if args.print_time:
-            print(datetime.now().timestamp(), end=' ')
-        print("---Sent---:" if is_send else ("-Received-:" if is_receive else "UNKNOWN SOURCE: "))
-        if args.very_verbose:
-            print(line, end='')
-        if args.verbose:
-            print(data)
-            print(decimals)
-
-        if args.only_print:
-            print(chars)
-            continue
-
-        print('{ ServiceID:', service_id, 'CommandID:', command_id, '}')
-        print('Data:', decimals[6:-2])
-
-        if args.print:
-            print('=== Printable ===')
-            print(chars)
-            print('=================')
-        print()
+        last_payload = ""
+        _smart_divided = False
+        while len(data) >= 14:
+            decimals = print_hex_data.string_to_decimals(data)
+            length = decimals[2] + 5  # get lenght byte and add 5 for rest of bytes that it doesn't include
+            if length < len(decimals):
+                _smart_divided = True  # length shows to be smaller than what we have - we will smart divide
+            hex_str_length = length * 2  # every byte is two hex chars
+            actual_payload = data[:hex_str_length]
+            data = data[hex_str_length:]  # Shorten data by what we have already parsed
+            if actual_payload == last_payload:
+                continue  # Skip if it's same as last (they often repeat)
+            last_payload = actual_payload
+            handle_payload(line, actual_payload, _is_send, _is_receive, _smart_divided)
