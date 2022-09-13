2b4699c639ddd5c04116e82be3a89e68b5a3007e
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-09-13 22:30:31 +0200

    Specify headlines

--- lib/ui/theme/themes.dart
+++ lib/ui/theme/themes.dart
@@ -11,20 +11,24 @@ ThemeData get _dark => ThemeData.dark(useMaterial3: useMaterial3);
 ThemeData _customize(ThemeData theme) {
   return theme.copyWith(
     textTheme: theme.textTheme.copyWith(
+      // Body
       bodyMedium: theme.textTheme.bodyMedium!.copyWith(
         fontSize: 15.0,
       ),
       bodyLarge: theme.textTheme.bodyLarge!.copyWith(
         fontSize: 17.0,
       ),
+      // Headlines
       headlineSmall: theme.textTheme.headlineSmall!.copyWith(
         fontSize: 20.0,
       ),
       headlineMedium: theme.textTheme.headlineMedium!.copyWith(
         color: theme.textTheme.bodyMedium!.color,
+        fontSize: 28.0,
       ),
       headlineLarge: theme.textTheme.headlineLarge!.copyWith(
         color: theme.textTheme.bodyMedium!.color,
+        fontSize: 32.0,
       ),
     ),
   );
