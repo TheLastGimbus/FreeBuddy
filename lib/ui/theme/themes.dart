680511410c0a457c1f18fb981b0a59a389da8746
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-09-13 21:56:08 +0200

    :)

--- lib/ui/theme/themes.dart
+++ lib/ui/theme/themes.dart
@@ -7,35 +7,39 @@ ThemeData get _light => ThemeData.light(useMaterial3: useMaterial3);
 
 ThemeData get _dark => ThemeData.dark(useMaterial3: useMaterial3);
 
-// TODO: Some way to merge overriding this two both
-ThemeData get lightTheme {
-  return _light.copyWith(
-    textTheme: _light.textTheme.copyWith(
-      bodyMedium: _light.textTheme.bodyMedium!.copyWith(
+/// This allows us to override both themes
+ThemeData _customize(ThemeData theme) {
+  return theme.copyWith(
+    textTheme: theme.textTheme.copyWith(
+      bodyMedium: theme.textTheme.bodyMedium!.copyWith(
         fontSize: 15.0,
       ),
-      bodyLarge: _light.textTheme.bodyLarge!.copyWith(
+      bodyLarge: theme.textTheme.bodyLarge!.copyWith(
         fontSize: 17.0,
       ),
-      headlineLarge: _light.textTheme.headlineLarge!.copyWith(
-        color: _light.textTheme.bodyMedium!.color,
+      headlineLarge: theme.textTheme.headlineLarge!.copyWith(
+        color: theme.textTheme.bodyMedium!.color,
       ),
     ),
   );
 }
 
+ThemeData get lightTheme {
+  return _customize(_light).copyWith(
+    colorScheme: _light.colorScheme.copyWith(
+        // Leaving this so you see how you can customize colors individually
+        // primary: const Color(0xFF1E88E5),
+        // secondary: const Color(0xFF1E88E5),
+        ),
+  );
+}
+
 ThemeData get darkTheme {
-  return _dark.copyWith(
-    textTheme: _dark.textTheme.copyWith(
-      bodyMedium: _dark.textTheme.bodyMedium!.copyWith(
-        fontSize: 15.0,
-      ),
-      bodyLarge: _dark.textTheme.bodyLarge!.copyWith(
-        fontSize: 17.0,
-      ),
-      headlineLarge: _dark.textTheme.headlineLarge!.copyWith(
-        color: _dark.textTheme.bodyMedium!.color,
-      ),
-    ),
+  return _customize(_dark).copyWith(
+    colorScheme: _dark.colorScheme.copyWith(
+        // Leaving this so you see how you can customize colors individually
+        // primary: const Color(0xFF1E88E5),
+        // secondary: const Color(0xFF1E88E5),
+        ),
   );
 }
