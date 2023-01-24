4fd58b52c02f7b571bf9b0fd249cabdbbfa794da
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-25 00:49:00 +0100

    reeefactooor

--- lib/headphones/headphones_service/headphones_service_bluetooth.dart
+++ lib/headphones/headphones_service/headphones_service_bluetooth.dart
@@ -15,67 +15,76 @@ class HeadphonesConnectedOpenPlugin implements HeadphonesConnectedOpen {
       StreamController<HeadphonesBatteryData>.broadcast();
 
   HeadphonesConnectedOpenPlugin(this.connection) {
-    connection.io.stream.listen(
-      (event) {
-        List<MbbCommand>? comms;
+    connection.io.stream.listen((event) {
+      List<MbbCommand>? commands;
+      try {
+        commands = MbbCommand.fromPayload(event);
+      } catch (e, s) {
+        logg.e("mbb parsing error", e, s);
+      }
+      for (final cmd in commands ?? []) {
+        logg.v("Received mbb cmd: $cmd");
         try {
-          comms = MbbCommand.fromPayload(event);
-        } catch (e, s) {
-          logg.e("MBB parsing error", e, s);
+          _evalMbbCommand(cmd);
+        } on RangeError catch (e, s) {
+          logg.e(
+              'Error while parsing mbb cmd - (probably missing bytes)', e, s);
         }
-        if (comms == null) return;
-        for (final comm in comms) {
-          logg.v("Received mbb comm: $comm");
-
-          if (comm.serviceId == 43 &&
-              comm.commandId == 42 &&
-              comm.args.containsKey(1)) {
-            late HeadphonesAncMode newMode;
-            // TODO: Add some constants for this globally
-            // because 0 1 and 2 seem to be constant bytes representing the modes
-            switch (comm.args[1]?[1] ?? -1) {
-              case 1:
-                newMode = HeadphonesAncMode.noiseCancel;
-                break;
-              case 0:
-                newMode = HeadphonesAncMode.off;
-                break;
-              case 2:
-                newMode = HeadphonesAncMode.awareness;
-                break;
-              default:
-                logg.e("Unknown ANC mode: ${comm.args[1]}");
-                continue;
-            }
-            _ancStreamCtrl.add(newMode);
-          }
-          if (comm.serviceId == 1 &&
-              (comm.commandId == 39 || comm.commandId == 8) &&
-              comm.args.length == 3) {
-            final level = comm.args[2];
-            final status = comm.args[3];
-            if (level == null || status == null) {
-              logg.e("Battery data is missing level or status");
-              continue;
-            }
-            _batteryStreamCtrl.add(HeadphonesBatteryData(
-              level[0] == 0 ? null : level[0],
-              level[1] == 0 ? null : level[1],
-              level[2] == 0 ? null : level[2],
-              status[0] == 1,
-              status[1] == 1,
-              status[2] == 1,
-            ));
-          }
-        }
-      },
-    );
+      }
+    });
     _initRequestInfo();
+  }
+
+  void _evalMbbCommand(MbbCommand cmd) {
+    if (cmd.serviceId == 43 && cmd.commandId == 42 && cmd.args.containsKey(1)) {
+      late HeadphonesAncMode newMode;
+      // TODO: Add some constants for this globally
+      // because 0 1 and 2 seem to be constant bytes representing the modes
+      final modeByte = cmd.args[1]![1];
+      if (modeByte == 1) {
+        newMode = HeadphonesAncMode.noiseCancel;
+      } else if (modeByte == 0) {
+        newMode = HeadphonesAncMode.off;
+      } else if (modeByte == 2) {
+        newMode = HeadphonesAncMode.awareness;
+      } else {
+        logg.e("Unknown ANC mode: ${cmd.args[1]}");
+        return;
+      }
+      _ancStreamCtrl.add(newMode);
+    } else if (cmd.serviceId == 1 &&
+        (cmd.commandId == 39 || cmd.commandId == 8) &&
+        cmd.args.length >= 3) {
+      final level = cmd.args[2];
+      final status = cmd.args[3];
+      if (level == null || status == null) {
+        logg.e("Battery data is missing level or status");
+        return;
+      }
+      _batteryStreamCtrl.add(HeadphonesBatteryData(
+        level[0] == 0 ? null : level[0],
+        level[1] == 0 ? null : level[1],
+        level[2] == 0 ? null : level[2],
+        status[0] == 1,
+        status[1] == 1,
+        status[2] == 1,
+      ));
+    }
   }
 
   Future<void> _initRequestInfo() async {
     await _sendMbb(MbbCommand.requestBattery);
     await _sendMbb(MbbCommand.requestAnc);
+  }
+
+  // TODO: some .flush() for those two
+
+  Future<void> _sendMbb(MbbCommand comm) async {
+    connection.io.sink.add(comm.toPayload());
+  }
+
+  Future<void> sendCustomMbbCommand(MbbCommand comm) async {
+    connection.io.sink.add(comm.toPayload());
   }
 
   @override
@@ -99,15 +108,5 @@ class HeadphonesConnectedOpenPlugin implements HeadphonesConnectedOpen {
         break;
     }
     await _sendMbb(comm);
-  }
-
-  // TODO: some .flush() for those two
-
-  Future<void> _sendMbb(MbbCommand comm) async {
-    connection.io.sink.add(comm.toPayload());
-  }
-
-  Future<void> sendCustomMbbCommand(MbbCommand comm) async {
-    connection.io.sink.add(comm.toPayload());
   }
 }
