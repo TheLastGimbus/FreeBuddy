b7e01ed08ac0a8f177208389f18ba1486ab5eaa2
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-24 17:11:10 +0100

    try catch

--- lib/headphones/headphones_connection_cubit.dart
+++ lib/headphones/headphones_connection_cubit.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 
 import 'package:app_settings/app_settings.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_last_bluetooth/the_last_bluetooth.dart';
 
-import '../logger.dart';
 import 'headphones_service/headphones_service_base.dart';
 import 'headphones_service/headphones_service_bluetooth.dart';
 import 'otter_constants.dart';
@@ -40,16 +40,19 @@ class HeadphonesConnectionCubit extends Cubit<HeadphonesObject> {
         return;
       }
       emit(HeadphonesConnecting());
-      _connection = await bluetooth.connectRfcomm(otter!, sppUuid);
-      emit(HeadphonesConnectedOpenPlugin(_connection!));
-      logg.d(
-          "connected to otter: isBroadcast: ${_connection!.io.stream.isBroadcast}");
-      await _connection!.io.stream.listen((event) {}).asFuture();
-      // when device disconnects, future completes and we free the
-      // hopefully this happens *before* next stream event with data 🤷
-      // so that it nicely goes again and we emit HeadphonesDisconnected()
-      _connection = null;
-      emit(HeadphonesConnectedClosed());
+      try {
+        _connection = await bluetooth.connectRfcomm(otter!, sppUuid);
+        emit(HeadphonesConnectedOpenPlugin(_connection!));
+        await _connection!.io.stream.listen((event) {}).asFuture();
+        // when device disconnects, future completes and we free the
+        // hopefully this happens *before* next stream event with data 🤷
+        // so that it nicely goes again and we emit HeadphonesDisconnected()
+      } on PlatformException catch (_) {
+      } finally {
+        await _connection?.io.sink.close();
+        _connection = null;
+        emit(HeadphonesConnectedClosed());
+      }
     });
   }
 
@@ -70,9 +73,14 @@ class HeadphonesConnectionCubit extends Cubit<HeadphonesObject> {
       return false;
     }
     emit(HeadphonesConnecting());
-    _connection = await bluetooth.connectRfcomm(otter!, sppUuid);
-    emit(HeadphonesConnectedOpenPlugin(_connection!));
-    return true;
+    try {
+      _connection = await bluetooth.connectRfcomm(otter!, sppUuid);
+      emit(HeadphonesConnectedOpenPlugin(_connection!));
+      return true;
+    } on PlatformException catch (_) {
+      emit(HeadphonesConnectedClosed());
+      return false;
+    }
   }
 
   HeadphonesConnectionCubit({required this.bluetooth})
