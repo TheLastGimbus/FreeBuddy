615290eb3dc95bbfad92c4c806de878b7037337d
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-27 00:52:54 +0100

    animate!
    also fix animations and some bugs that probably where there :)

--- lib/ui/pages/disabled.dart
+++ lib/ui/pages/disabled.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 /// the [child] so that you can show user why it's disabled
 class Disabled extends StatelessWidget {
   final Widget child;
+  static const _fadeDuration = Duration(milliseconds: 350);
 
   /// Aww, fuck it
   final bool disabled;
@@ -21,28 +22,34 @@ class Disabled extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final t = Theme.of(context);
-    return disabled
-        ? AbsorbPointer(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Opacity(opacity: 0.45, child: child),
-                if (coveringWidget != null)
-                  Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: t.colorScheme.background,
-                          spreadRadius: 14,
-                          blurRadius: 22,
-                        ),
-                      ],
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        AnimatedOpacity(
+          opacity: disabled ? 0.45 : 1,
+          duration: _fadeDuration,
+          child: AbsorbPointer(absorbing: disabled, child: child),
+        ),
+        if (coveringWidget != null)
+          IgnorePointer(
+            child: AnimatedOpacity(
+              opacity: disabled ? 1 : 0,
+              duration: _fadeDuration,
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: t.colorScheme.background,
+                      spreadRadius: 14,
+                      blurRadius: 22,
                     ),
-                    child: coveringWidget!,
-                  ),
-              ],
+                  ],
+                ),
+                child: coveringWidget!,
+              ),
             ),
-          )
-        : child;
+          ),
+      ],
+    );
   }
 }
