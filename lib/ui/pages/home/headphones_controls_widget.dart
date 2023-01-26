1a612be8f38c31bdb8a96755ccfb622c45826511
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-26 23:33:38 +0100

    move this out so it's easier to edit

--- lib/ui/pages/home/headphones_controls_widget.dart
+++ lib/ui/pages/home/headphones_controls_widget.dart
@@ -31,119 +31,158 @@ class HeadphonesControlsWidget extends StatelessWidget {
         ),
         Row(
           children: [
-            Expanded(
-              child: PrettyRoundedContainerWidget(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Text('Sleep mode'),
-                    StreamBuilder<bool>(
-                      stream: context.read<AppSettings>().sleepMode,
-                      builder: (context, snapshot) => Switch(
-                        value: snapshot.data ?? false,
-                        onChanged: (value) async {
-                          // TODO: move this logic somewhere else 🙏
-                          // currently have no idea where
-                          // maybe some "CoolModes" class 0_o
-                          final settings = context.read<AppSettings>();
-                          if (value) {
-                            settings.setSleepModePreviousSettings(
-                              await headphones.dumpSettings(),
-                            );
-                            await headphones.setAncMode(HeadphonesAncMode.off);
-                            await headphones.setAutoPause(false);
-                            // TODO: Disable gestures
-                            // TODO #2 electric boogaloo: implement gestures
-                          } else {
-                            await headphones.restoreSettings(
-                              await settings.sleepModePreviousSettings.first,
-                            );
-                          }
-                          settings.setSleepMode(value);
-                          // TODO: Disable widgets below when sleep mode
-                          // and tell user whats going on
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            Expanded(child: _SleepModeSwitch(headphones)),
             const SizedBox(width: 16),
-            Expanded(
-              child: PrettyRoundedContainerWidget(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Text("Auto pause"),
-                    StreamBuilder<bool>(
-                      stream: headphones.autoPause,
-                      builder: (context, snapshot) => Switch(
-                        value: snapshot.data ?? false,
-                        onChanged: headphones.setAutoPause,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            Expanded(child: _AutoPauseSwitch(headphones)),
           ],
         ),
         const SizedBox(height: 16.0),
-        PrettyRoundedContainerWidget(
-          child: StreamBuilder<HeadphonesBatteryData>(
-            initialData:
-                HeadphonesBatteryData(null, null, null, false, false, false),
-            stream: headphones.batteryData,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return const Text(":(");
-              final b = snapshot.data!;
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  BatteryCircleWidget(
-                      b.levelLeft, b.lowestLevel, b.chargingLeft, "L"),
-                  BatteryCircleWidget(
-                      b.levelRight, b.lowestLevel, b.chargingRight, "R"),
-                  BatteryCircleWidget(
-                      b.levelCase, b.lowestLevel, b.chargingCase, "C"),
-                ],
-              );
-            },
-          ),
-        ),
+        _BatteryInfoRow(headphones),
         const SizedBox(height: 16.0),
-        PrettyRoundedContainerWidget(
-          child: StreamBuilder<HeadphonesAncMode>(
-            stream: headphones.ancMode,
-            builder: (context, snapshot) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  AncButtonWidget(
-                    icon: Fms.noise_control_on,
-                    isSelected: snapshot.data == HeadphonesAncMode.noiseCancel,
-                    onPressed: () =>
-                        headphones.setAncMode(HeadphonesAncMode.noiseCancel),
-                  ),
-                  AncButtonWidget(
-                    icon: Fms.noise_control_off,
-                    isSelected: snapshot.data == HeadphonesAncMode.off,
-                    onPressed: () =>
-                        headphones.setAncMode(HeadphonesAncMode.off),
-                  ),
-                  AncButtonWidget(
-                    icon: Fms.noise_aware,
-                    isSelected: snapshot.data == HeadphonesAncMode.awareness,
-                    onPressed: () =>
-                        headphones.setAncMode(HeadphonesAncMode.awareness),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
+        _AncControlRow(headphones),
       ],
+    );
+  }
+}
+
+class _BatteryInfoRow extends StatelessWidget {
+  final HeadphonesConnectedOpen headphones;
+
+  const _BatteryInfoRow(this.headphones);
+
+  @override
+  Widget build(BuildContext context) {
+    return PrettyRoundedContainerWidget(
+      child: StreamBuilder<HeadphonesBatteryData>(
+        initialData:
+            HeadphonesBatteryData(null, null, null, false, false, false),
+        stream: headphones.batteryData,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text(":(");
+          final b = snapshot.data!;
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              BatteryCircleWidget(
+                  b.levelLeft, b.lowestLevel, b.chargingLeft, "L"),
+              BatteryCircleWidget(
+                  b.levelRight, b.lowestLevel, b.chargingRight, "R"),
+              BatteryCircleWidget(
+                  b.levelCase, b.lowestLevel, b.chargingCase, "C"),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _AncControlRow extends StatelessWidget {
+  final HeadphonesConnectedOpen headphones;
+
+  const _AncControlRow(this.headphones);
+
+  @override
+  Widget build(BuildContext context) {
+    return PrettyRoundedContainerWidget(
+      child: StreamBuilder<HeadphonesAncMode>(
+        stream: headphones.ancMode,
+        builder: (context, snapshot) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              AncButtonWidget(
+                icon: Fms.noise_control_on,
+                isSelected: snapshot.data == HeadphonesAncMode.noiseCancel,
+                onPressed: () =>
+                    headphones.setAncMode(HeadphonesAncMode.noiseCancel),
+              ),
+              AncButtonWidget(
+                icon: Fms.noise_control_off,
+                isSelected: snapshot.data == HeadphonesAncMode.off,
+                onPressed: () => headphones.setAncMode(HeadphonesAncMode.off),
+              ),
+              AncButtonWidget(
+                icon: Fms.noise_aware,
+                isSelected: snapshot.data == HeadphonesAncMode.awareness,
+                onPressed: () =>
+                    headphones.setAncMode(HeadphonesAncMode.awareness),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _AutoPauseSwitch extends StatelessWidget {
+  final HeadphonesConnectedOpen headphones;
+
+  const _AutoPauseSwitch(this.headphones);
+
+  @override
+  Widget build(BuildContext context) {
+    return PrettyRoundedContainerWidget(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Text("Auto pause"),
+          StreamBuilder<bool>(
+            stream: headphones.autoPause,
+            builder: (context, snapshot) => Switch(
+              value: snapshot.data ?? false,
+              onChanged: headphones.setAutoPause,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SleepModeSwitch extends StatelessWidget {
+  final HeadphonesConnectedOpen headphones;
+
+  const _SleepModeSwitch(this.headphones);
+
+  @override
+  Widget build(BuildContext context) {
+    return PrettyRoundedContainerWidget(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Text('Sleep mode'),
+          StreamBuilder<bool>(
+            stream: context.read<AppSettings>().sleepMode,
+            builder: (context, snapshot) => Switch(
+              value: snapshot.data ?? false,
+              onChanged: (value) async {
+                // TODO: move this logic somewhere else 🙏
+                // currently have no idea where
+                // maybe some "CoolModes" class 0_o
+                final settings = context.read<AppSettings>();
+                if (value) {
+                  settings.setSleepModePreviousSettings(
+                    await headphones.dumpSettings(),
+                  );
+                  await headphones.setAncMode(HeadphonesAncMode.off);
+                  await headphones.setAutoPause(false);
+                  // TODO: Disable gestures
+                  // TODO #2 electric boogaloo: implement gestures
+                } else {
+                  await headphones.restoreSettings(
+                    await settings.sleepModePreviousSettings.first,
+                  );
+                }
+                settings.setSleepMode(value);
+                // TODO: Disable widgets below when sleep mode
+                // and tell user whats going on
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
