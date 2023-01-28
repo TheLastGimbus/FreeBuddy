8022d0da0fb039bb19016e6e3907263a6db30c5e
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-28 20:22:19 +0100

    Prettify bt disabled

--- lib/ui/pages/home/bluetooth_disabled_info_widget.dart
+++ lib/ui/pages/home/bluetooth_disabled_info_widget.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../../headphones/cubit/headphones_connection_cubit.dart';
 import '../pretty_rounded_container_widget.dart';
 
 class BluetoothDisabledInfoWidget extends StatelessWidget {
-  final VoidCallback? onEnable;
-  final VoidCallback? onOpenSettings;
-
-  const BluetoothDisabledInfoWidget(
-      {Key? key, this.onEnable, this.onOpenSettings})
-      : super(key: key);
+  const BluetoothDisabledInfoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final l = AppLocalizations.of(context)!;
+    final t = Theme.of(context);
+    final tt = t.textTheme;
     return PrettyRoundedContainerWidget(
-      child: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(l.pageHomeBluetoothDisabled),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              l.pageHomeBluetoothDisabled,
+              style: tt.displaySmall,
+              textAlign: TextAlign.center,
             ),
-            TextButton(
-              onPressed: onEnable,
-              child: Text(l.pageHomeBluetoothDisabledEnable),
-            ),
-            TextButton(
-              onPressed: onOpenSettings,
-              child: Text(l.pageHomeBluetoothDisabledEnableOpenSettings),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 16),
+          // the_last_bluetooth plugin doesn't support this for now
+          // TextButton(
+          //   onPressed: onEnable,
+          //   child: Text(l.pageHomeBluetoothDisabledEnable),
+          // ),
+          FilledButton(
+            onPressed: () => context
+                .read<HeadphonesConnectionCubit>()
+                .openBluetoothSettings(),
+            child: Text(l.pageHomeBluetoothDisabledOpenSettings),
+          ),
+        ],
       ),
     );
   }
