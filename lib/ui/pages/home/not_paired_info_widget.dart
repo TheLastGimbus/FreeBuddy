0dd3e047b282b701dabc123dca849df8e5070c35
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-28 20:07:32 +0100

    Prettify not paired info

--- lib/ui/pages/home/not_paired_info_widget.dart
+++ lib/ui/pages/home/not_paired_info_widget.dart
@@ -1,7 +1,9 @@
-import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
+import '../../../headphones/cubit/headphones_connection_cubit.dart';
 import '../pretty_rounded_container_widget.dart';
 
 class NotPairedInfoWidget extends StatelessWidget {
@@ -11,21 +13,27 @@ class NotPairedInfoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final l = AppLocalizations.of(context)!;
     return PrettyRoundedContainerWidget(
-      child: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(l.pageHomeNotPaired),
-            ),
-            TextButton(
-              child: Text(l.pageHomeNotPairedPairOpenSettings),
-              onPressed: () {
-                AppSettings.openBluetoothSettings(asAnotherTask: true);
-              },
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(l.pageHomeNotPaired, textAlign: TextAlign.center),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            child: Text(l.pageHomeNotPairedPairOpenSettings),
+            onPressed: () => context
+                .read<HeadphonesConnectionCubit>()
+                .openBluetoothSettings(),
+          ),
+          const SizedBox(height: 16),
+          TextButton(
+            child: Text(l.pageHomeNotPairedPairOpenDemo),
+            onPressed: () =>
+                launchUrlString('https://freebuddy-web-demo.netlify.app/'),
+          )
+        ],
       ),
     );
   }
