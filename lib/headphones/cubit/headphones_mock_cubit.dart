81d19a262a20ee2dc609369b6663611abd678c5d
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-01-28 16:29:20 +0100

    make separate mocks file for more mocks!

--- lib/headphones/cubit/headphones_mock_cubit.dart
+++ lib/headphones/cubit/headphones_mock_cubit.dart
@@ -1,16 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rxdart/rxdart.dart';
 
-import '../headphones_base.dart';
-import '../headphones_data_objects.dart';
+import '../headphones_mocks.dart';
 import 'headphones_connection_cubit.dart';
 import 'headphones_cubit_objects.dart';
 
 class HeadphonesMockCubit extends Cubit<HeadphonesConnectionState>
     implements HeadphonesConnectionCubit {
-  HeadphonesMockCubit() : super(HeadphonesConnectedOpen(HeadphonesMock()));
+  HeadphonesMockCubit()
+      : super(HeadphonesConnectedOpen(HeadphonesMockPrettyFake()));
 
   @override
   Future<void> connect() async {}
@@ -20,42 +19,4 @@ class HeadphonesMockCubit extends Cubit<HeadphonesConnectionState>
 
   @override
   Future<void> openBluetoothSettings() async {}
-}
-
-class HeadphonesMock extends HeadphonesBase {
-  final _batteryData = BehaviorSubject<HeadphonesBatteryData>();
-  final _ancMode = BehaviorSubject<HeadphonesAncMode>();
-  final _autoPause = BehaviorSubject<bool>();
-
-  HeadphonesMock() {
-    Stream.periodic(
-      const Duration(seconds: 1),
-      (i) => HeadphonesBatteryData(
-        ((i * 1.0 - 100).abs() % 100).round(),
-        ((i * 1.1 - 100).abs() % 100).round(),
-        ((i * 0.7 - 100).abs() % 100).round(),
-        i % 35 < 10,
-        (i + 6) % 35 < 10,
-        i % 15 < 10,
-      ),
-    ).listen(_batteryData.add);
-    _batteryData.add(HeadphonesBatteryData(100, 100, 100, true, true, true));
-    _ancMode.add(HeadphonesAncMode.off);
-    _autoPause.add(false);
-  }
-
-  @override
-  ValueStream<HeadphonesBatteryData> get batteryData => _batteryData.stream;
-
-  @override
-  ValueStream<HeadphonesAncMode> get ancMode => _ancMode.stream;
-
-  @override
-  Future<void> setAncMode(HeadphonesAncMode mode) async => _ancMode.add(mode);
-
-  @override
-  ValueStream<bool> get autoPause => _autoPause.stream;
-
-  @override
-  Future<void> setAutoPause(bool enabled) async => _autoPause.add(enabled);
 }
---
+++ lib/headphones/headphones_mocks.dart
@@ -0,0 +1,63 @@
+import 'package:rxdart/rxdart.dart';
+
+import 'headphones_base.dart';
+import 'headphones_data_objects.dart';
+
+/// Headphones that never have any data
+class HeadphonesMockNever extends HeadphonesBase {
+  @override
+  ValueStream<HeadphonesBatteryData> get batteryData =>
+      NeverStream<HeadphonesBatteryData>().shareValue();
+
+  @override
+  ValueStream<HeadphonesAncMode> get ancMode =>
+      NeverStream<HeadphonesAncMode>().shareValue();
+
+  @override
+  Future<void> setAncMode(HeadphonesAncMode mode) async {}
+
+  @override
+  ValueStream<bool> get autoPause => NeverStream<bool>().shareValue();
+
+  @override
+  Future<void> setAutoPause(bool enabled) async {}
+}
+
+/// Pretty faked headphones that emit some different fake values over time
+class HeadphonesMockPrettyFake extends HeadphonesBase {
+  final _batteryData = BehaviorSubject<HeadphonesBatteryData>();
+  final _ancMode = BehaviorSubject<HeadphonesAncMode>();
+  final _autoPause = BehaviorSubject<bool>();
+
+  HeadphonesMockPrettyFake() {
+    Stream.periodic(
+      const Duration(seconds: 1),
+      (i) => HeadphonesBatteryData(
+        ((i * 1.0 - 100).abs() % 100).round(),
+        ((i * 1.1 - 100).abs() % 100).round(),
+        ((i * 0.7 - 100).abs() % 100).round(),
+        i % 35 < 10,
+        (i + 6) % 35 < 10,
+        i % 15 < 10,
+      ),
+    ).listen(_batteryData.add);
+    _batteryData.add(HeadphonesBatteryData(100, 100, 100, true, true, true));
+    _ancMode.add(HeadphonesAncMode.off);
+    _autoPause.add(false);
+  }
+
+  @override
+  ValueStream<HeadphonesBatteryData> get batteryData => _batteryData.stream;
+
+  @override
+  ValueStream<HeadphonesAncMode> get ancMode => _ancMode.stream;
+
+  @override
+  Future<void> setAncMode(HeadphonesAncMode mode) async => _ancMode.add(mode);
+
+  @override
+  ValueStream<bool> get autoPause => _autoPause.stream;
+
+  @override
+  Future<void> setAutoPause(bool enabled) async => _autoPause.add(enabled);
+}
