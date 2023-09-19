f63f3ac07ae6e4d3acfaaa447424435a4088e860
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-09-19 13:32:16 +0200

    cleanups
    i want to merge this to master bc i feel bad with holding such big branch separate for so long - and chaning .fvm_config with it etc

--- lib/ui/appwidgets/battery_appwidget.dart
+++ lib/ui/appwidgets/battery_appwidget.dart
@@ -3,18 +3,15 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:screenshot/screenshot.dart';
 
 import '../../headphones/cubit/headphones_cubit_objects.dart';
 import '../../headphones/headphones_data_objects.dart';
-import 'battery_appwidget_widget.dart';
 
 // no better idea for this yet - that's fine
 StreamSubscription<HeadphonesBatteryData>? _headphonesBatteryStreamSub;
 
 @pragma("vm:entry-point")
-void updateBatteryAppwidget() {
-}
+void updateBatteryAppwidget() {}
 
 void batteryHomeWidgetHearBloc(BuildContext context,
     HeadphonesConnectionState headphonesConnectionState) async {
