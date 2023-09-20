bcf0a8c9535eaa7050e068c42ec33f2894c286b0
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-09-20 15:19:01 +0200

    always print everything
    will have to watch out for personal data now :eyes:

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,7 +1,9 @@
 import 'package:logger/logger.dart';
 
 final _prettyLogger = Logger(
+  filter: ProductionFilter(),
   printer: PrettyPrinter(),
+  level: Level.all,
 );
 
 Logger get logg => _prettyLogger;
