cb0a2454c5e4884f3716b05bfabdf2a77e56a5aa
TheLastGimbus <mateusz.soszynski@tuta.io>
2024-02-23 20:42:40 +0100

    there we go, good linter :>

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -22,15 +22,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _introCheck() async {
-    final settings = context.read<AppSettings>();
+    final ctx = context;
+    final settings = ctx.read<AppSettings>();
     if (!(await settings.seenIntroduction.first)) {
       // https://dart-lang.github.io/linter/lints/use_build_context_synchronously.html
-      if (!context.mounted) return;
+      if (!ctx.mounted) return;
       // true from this route means all success and we can set the flag
       // false means user exited otherwise or smth - anyway, don't set the flag
       final success =
-          // ignore: use_build_context_synchronously
-          await Navigator.of(context).pushNamed('/introduction') as bool?;
+          await Navigator.of(ctx).pushNamed('/introduction') as bool?;
       if (success ?? false) {
         await settings.setSeenIntroduction(true);
       }
