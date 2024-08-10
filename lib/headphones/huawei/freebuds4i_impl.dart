30f6830f73929f814a25b40e9259078ff01af952
TheLastGimbus <mateusz.soszynski@tuta.io>
2024-08-10 14:03:17 +0200

    4i working fully, now just make them clearer

--- lib/headphones/huawei/freebuds4i_impl.dart
+++ lib/headphones/huawei/freebuds4i_impl.dart
@@ -85,8 +85,9 @@ final class HuaweiFreeBuds4iImpl extends HuaweiFreeBuds4i {
   // TODO: Decide how we treat exceptions here
   void _evalMbbCommand(MbbCommand cmd) {
     // TODO/MISSING: Gesture settings
-    // final lastGestures = _gestureSettingsStreamCtrl.valueOrNull ??
-    //     const HeadphonesGestureSettings();
+    final lastSettings =
+        _settingsCtrl.valueOrNull ?? const HuaweiFreeBuds4iSettings();
+    // todo: try pattern matching here
     if (cmd.isAbout(_Cmd.getAnc) && cmd.args.containsKey(1)) {
       // 0 1 and 2 seem to be constant bytes representing the modes
       _ancModeCtrl.add(switch (cmd.args[1]![1]) {
@@ -101,7 +102,7 @@ final class HuaweiFreeBuds4iImpl extends HuaweiFreeBuds4i {
       final level = cmd.args[2];
       final status = cmd.args[3];
       if (level == null || status == null) {
-        logg.e("Battery data is missing level or status");
+        logg.w("Battery data is missing level or status");
         return;
       }
       _lrcBatteryCtrl.add(LRCBatteryLevels(
@@ -112,52 +113,48 @@ final class HuaweiFreeBuds4iImpl extends HuaweiFreeBuds4i {
         status[1] == 1,
         status[2] == 1,
       ));
-    } else if (cmd.serviceId == 43 &&
-        cmd.commandId == 17 &&
-        cmd.args.containsKey(1)) {
-      // TODO/MISSING: Auto pause settings
-      // _autoPauseStreamCtrl.add(cmd.args[1]![0] == 1);
-    } else if (cmd.serviceId == 1 && cmd.commandId == 32) {
-      // TODO/MISSING: Gesture settings
-      // _gestureSettingsStreamCtrl.add(
-      //   lastGestures.copyWith(
-      //     doubleTapLeft: cmd.args[1] != null
-      //         ? HeadphonesGestureDoubleTap.fromMbbValue(cmd.args[1]![0])
-      //         : lastGestures.doubleTapLeft,
-      //     doubleTapRight: cmd.args[2] != null
-      //         ? HeadphonesGestureDoubleTap.fromMbbValue(cmd.args[2]![0])
-      //         : lastGestures.doubleTapRight,
-      //   ),
-      // );
-    } else if ((cmd.serviceId == 43 && cmd.commandId == 23)) {
-      // TODO/MISSING: Gesture settings
-      // _gestureSettingsStreamCtrl.add(
-      //   lastGestures.copyWith(
-      //     holdBoth: (cmd.args[1] != null)
-      //         ? HeadphonesGestureHold.fromMbbValue(cmd.args[1]![0])
-      //         : lastGestures.holdBoth,
-      //   ),
-      // );
-    } else if (cmd.serviceId == 43 && cmd.commandId == 25) {
-      // TODO/MISSING: Gesture settings
-      // _gestureSettingsStreamCtrl.add(
-      //   lastGestures.copyWith(
-      //     holdBothToggledAncModes: (cmd.args[1] != null)
-      //         ? gestureHoldFromMbbValue(cmd.args[1]![0])
-      //         : lastGestures.holdBothToggledAncModes,
-      //   ),
-      // );
+    } else if (cmd.isAbout(_Cmd.getAutoPause) && cmd.args.containsKey(1)) {
+      _settingsCtrl.add(lastSettings.copyWith(autoPause: cmd.args[1]![0] == 1));
+    } else if (cmd.isAbout(_Cmd.getGestureDoubleTap)) {
+      _settingsCtrl.add(
+        lastSettings.copyWith(
+          doubleTapLeft: cmd.args[1] != null
+              ? DoubleTap.values
+                  .firstWhereOrNull((e) => e.mbbCode == cmd.args[1]![0])
+              : lastSettings.doubleTapLeft,
+          doubleTapRight: cmd.args[2] != null
+              ? DoubleTap.values
+                  .firstWhereOrNull((e) => e.mbbCode == cmd.args[2]![0])
+              : lastSettings.doubleTapRight,
+        ),
+      );
+    } else if (cmd.isAbout(_Cmd.getGestureHold)) {
+      _settingsCtrl.add(
+        lastSettings.copyWith(
+          holdBoth: cmd.args[1] != null
+              ? Hold.values
+                  .firstWhereOrNull((e) => e.mbbCode == cmd.args[1]![0])
+              : lastSettings.holdBoth,
+        ),
+      );
+    } else if (cmd.isAbout(_Cmd.getGestureHoldToggledAncModes)) {
+      _settingsCtrl.add(
+        lastSettings.copyWith(
+          holdBothToggledAncModes: cmd.args[1] != null
+              ? _Cmd.gestureHoldToggledAncModesFromMbbValue(cmd.args[1]![0])
+              : lastSettings.holdBothToggledAncModes,
+        ),
+      );
     }
   }
 
   Future<void> _initRequestInfo() async {
     await _sendMbb(_Cmd.getBattery);
     await _sendMbb(_Cmd.getAnc);
-    // TODO/MISSING: Settings
-    // await _sendMbb(MbbCommand.requestAutoPause);
-    // await _sendMbb(MbbCommand.requestGestureDoubleTap);
-    // await _sendMbb(MbbCommand.requestGestureHold);
-    // await _sendMbb(MbbCommand.requestGestureHoldToggledAncModes);
+    await _sendMbb(_Cmd.getAutoPause);
+    await _sendMbb(_Cmd.getGestureDoubleTap);
+    await _sendMbb(_Cmd.getGestureHold);
+    await _sendMbb(_Cmd.getGestureHoldToggledAncModes);
   }
 
   // TODO: some .flush() for this
@@ -200,9 +197,39 @@ final class HuaweiFreeBuds4iImpl extends HuaweiFreeBuds4i {
   ValueStream<HuaweiFreeBuds4iSettings> get settings => _settingsCtrl.stream;
 
   @override
-  Future<void> setSettings(newSettings) {
-    // TODO: implement setSettings
-    throw UnimplementedError();
+  Future<void> setSettings(newSettings) async {
+    final prev = _settingsCtrl.valueOrNull ?? const HuaweiFreeBuds4iSettings();
+    // this is VERY much a boilerplate
+    // ...and, bloat...
+    // and i don't think there is a need to export it somewhere else 🤷,
+    // or make some other abstraction for it - maybe some day
+    if ((newSettings.doubleTapLeft ?? prev.doubleTapLeft) !=
+        prev.doubleTapLeft) {
+      await _sendMbb(_Cmd.gestureDoubleTapLeft(newSettings.doubleTapLeft!));
+      await _sendMbb(_Cmd.getGestureDoubleTap);
+    }
+    if ((newSettings.doubleTapRight ?? prev.doubleTapRight) !=
+        prev.doubleTapRight) {
+      await _sendMbb(_Cmd.gestureDoubleTapRight(newSettings.doubleTapRight!));
+      await _sendMbb(_Cmd.getGestureDoubleTap);
+    }
+    if ((newSettings.holdBoth ?? prev.holdBoth) != prev.holdBoth) {
+      await _sendMbb(_Cmd.gestureHold(newSettings.holdBoth!));
+      await _sendMbb(_Cmd.getGestureHold);
+      await _sendMbb(_Cmd.getGestureHoldToggledAncModes);
+    }
+    if ((newSettings.holdBothToggledAncModes ?? prev.holdBothToggledAncModes) !=
+        prev.holdBothToggledAncModes) {
+      await _sendMbb(_Cmd.gestureHoldToggledAncModes(
+          newSettings.holdBothToggledAncModes!));
+      await _sendMbb(_Cmd.getGestureHold);
+      await _sendMbb(_Cmd.getGestureHoldToggledAncModes);
+    }
+    if ((newSettings.autoPause ?? prev.autoPause) != prev.autoPause) {
+      await _sendMbb(
+          newSettings.autoPause! ? _Cmd.autoPauseOn : _Cmd.autoPauseOff);
+      await _sendMbb(_Cmd.getAutoPause);
+    }
   }
 }
 
@@ -217,77 +244,47 @@ abstract class _Cmd {
 
   static const getAnc = MbbCommand(43, 42);
 
-  static MbbCommand anc(AncMode mode) => switch (mode) {
-        AncMode.noiseCancelling => const MbbCommand(43, 4, {
-            1: [1, 255]
-          }),
-        AncMode.off => const MbbCommand(43, 4, {
-            1: [0, 0]
-          }),
-        AncMode.transparency => const MbbCommand(43, 4, {
-            1: [2, 255]
-          }),
-      };
+  static MbbCommand anc(AncMode mode) => MbbCommand(43, 4, {
+        1: [mode.mbbCode, mode == AncMode.off ? 0 : 255]
+      });
 
-  static const doubleTapMagic = {
-    DoubleTap.nothing: 255,
-    DoubleTap.voiceAssistant: 0,
-    DoubleTap.playPause: 1,
-    DoubleTap.next: 2,
-    DoubleTap.previous: 7
-  };
   static const getGestureDoubleTap = MbbCommand(1, 32);
 
   static MbbCommand gestureDoubleTapLeft(DoubleTap left) => MbbCommand(1, 31, {
-        1: [doubleTapMagic[left]!]
+        1: [left.mbbCode]
       });
 
   static MbbCommand gestureDoubleTapRight(DoubleTap right) =>
       MbbCommand(1, 31, {
-        2: [doubleTapMagic[right]!],
+        2: [right.mbbCode]
       });
-
-  static const holdMagic = {
-    Hold.nothing: 255,
-    Hold.cycleAnc: 10,
-  };
 
   static const getGestureHold = MbbCommand(43, 23);
 
   static MbbCommand gestureHold(Hold gestureHold) => MbbCommand(43, 22, {
-        1: [holdMagic[gestureHold]!],
+        1: [gestureHold.mbbCode]
       });
 
   static const getGestureHoldToggledAncModes = MbbCommand(43, 25);
 
-  static Set<AncMode> gestureHoldFromMbbValue(int mbbValue) {
-    switch (mbbValue) {
-      case 2:
-        return AncMode.values.toSet();
-      case 3:
-        return const {AncMode.off, AncMode.noiseCancelling};
-      case 5:
-        return const {AncMode.off, AncMode.transparency};
-      case 6:
-        return const {AncMode.noiseCancelling, AncMode.transparency};
-      case 255:
-        return {};
-      default:
-        throw Exception("Unknown mbbValue for $mbbValue");
-    }
+  static Set<AncMode>? gestureHoldToggledAncModesFromMbbValue(int mbbValue) {
+    return switch (mbbValue) {
+      1 => const {AncMode.off, AncMode.noiseCancelling},
+      2 => AncMode.values.toSet(),
+      3 => const {AncMode.noiseCancelling, AncMode.transparency},
+      4 => const {AncMode.off, AncMode.transparency},
+      255 => {},
+      _ => null,
+    };
   }
 
   static MbbCommand gestureHoldToggledAncModes(Set<AncMode> toggledModes) {
     int? mbbValue;
     const se = SetEquality();
-    if (![2, 3].contains(toggledModes.length)) {
-      throw Exception(
-          "toggledModes must have 2 or 3 elements, not ${toggledModes.length}}");
+    if (se.equals(toggledModes, {AncMode.off, AncMode.noiseCancelling})) {
+      mbbValue = 1;
     }
     if (toggledModes.length == 3) mbbValue = 2;
-    if (se.equals(toggledModes, {AncMode.off, AncMode.noiseCancelling})) {
-      mbbValue = 0;
-    }
     if (se.equals(
         toggledModes, {AncMode.noiseCancelling, AncMode.transparency})) {
       mbbValue = 3;
@@ -295,7 +292,11 @@ abstract class _Cmd {
     if (se.equals(toggledModes, {AncMode.off, AncMode.transparency})) {
       mbbValue = 4;
     }
-    if (mbbValue == null) throw Exception("Unknown mbbValue for $toggledModes");
+    if (mbbValue == null) {
+      logg.w("Unknown mbbValue for $toggledModes"
+          " - setting as 2 for 'all of them' as a recovery");
+      mbbValue = 2;
+    }
     return MbbCommand(43, 24, {
       1: [mbbValue],
       2: [mbbValue]
@@ -310,4 +311,29 @@ abstract class _Cmd {
   static const autoPauseOff = MbbCommand(43, 16, {
     1: [0]
   });
+}
+
+extension _FB4iAncMode on AncMode {
+  int get mbbCode => switch (this) {
+        AncMode.noiseCancelling => 1,
+        AncMode.off => 0,
+        AncMode.transparency => 2,
+      };
+}
+
+extension _FB4iDoubleTap on DoubleTap {
+  int get mbbCode => switch (this) {
+        DoubleTap.nothing => 255,
+        DoubleTap.voiceAssistant => 0,
+        DoubleTap.playPause => 1,
+        DoubleTap.next => 2,
+        DoubleTap.previous => 7
+      };
+}
+
+extension _FB4iHold on Hold {
+  int get mbbCode => switch (this) {
+        Hold.nothing => 255,
+        Hold.cycleAnc => 10,
+      };
 }
